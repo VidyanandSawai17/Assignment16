b2b4641d49d1ff8a80d5370c063092e24908b92f
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-01-25 16:51:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rect c1 = new rect(5,6);
+            c1.CalArea();
+            Console.WriteLine(c1);
+
+
+            /*
+             
+            Abstract class:
+
+            Abstract means which does not have implementation
+            If a class contains at least one abstract method, we need to declare an abstract keyword to the class. Class also becomes as an abstract class
+            When any child class inherits the abstract class , must provide the definition for an abstract method
+            All the abstract methods are by default virtual
+            We can not create object of an abstract class
+            */
+
+        }
+    }
+}
---
+++ cal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    abstract public class cal
+    {
+        public abstract void CalArea();  //  Abstract class: Abstract means which does not have implementation
+
+    }
+
+    public class rect : cal
+    {
+        private int l;
+        private int b;
+        private double r;
+
+        public rect(int l, int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public override void CalArea()
+        {
+            r = l * b;
+        }
+        public override string ToString()
+        {
+            return $"Area of Rectangle is {r}";
+        }
+
+
+
+
+
+    }
+
+}
